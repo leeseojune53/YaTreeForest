d51f6950e31abf96d3791e02d5dc04943c25eb34
leeseojune <sung07288346@gmail.com>
2021-03-24 19:44:27 +0900

    UPDATE; UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserDto } from './dto/user.dto';
+import { User } from './entity/user.entity';
+import * as bcrypt from 'bcrypt';
+import { UserRepository } from './entity/user.repo';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: UserRepository,
+    ){}
+
+    async register(request: UserDto){
+        if(await this.userRepository.findByEmail(request.email)){
+            throw new BadRequestException("Email is already registered");
+        }
+        if(await this.userRepository.findByUserName(request.userName)){
+            throw new BadRequestException("UserName is already registered");
+        }
+
+        request.password = await bcrypt.hash(request.password, 10); 
+        await this.userRepository.signUp(request);
+    }
+
+    
+
+}
