3ec084a51aca1eaa929228a2df18a1f2ea964fd4
leeseojune <sung07288346@gmail.com>
2021-03-24 19:43:38 +0900

    UPDATE; AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
-import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entity/user.entity';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      entities: [User],
+      synchronize: true
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, UserService],
+  providers: [AppService],
 })
 export class AppModule {}
