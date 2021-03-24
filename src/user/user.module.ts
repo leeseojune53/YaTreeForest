0af4906b354b6f8cc7745f4feb605f89bfaa73a0
leeseojune <sung07288346@gmail.com>
2021-03-24 19:44:19 +0900

    UPDATE; UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { UserRepository } from './entity/user.repo';
 
 @Module({
-  providers: [UserService]
+  imports: [TypeOrmModule.forFeature([UserRepository])],
+  providers: [UserService],
+  controllers: [UserController]
 })
 export class UserModule {}
