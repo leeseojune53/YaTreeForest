4e6e49e3e89c53662c5fa83bd82798c9fc3a93c4
leeseojune <sung07288346@gmail.com>
2021-03-25 20:12:12 +0900

    UPDATE; AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { UserDto } from './user/dto/user.dto';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
+    private readonly authService: AuthService,
   ){}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post()
+  login(@Body() request: UserDto) {
+    return this.authService.login(request.userName, request.password);
+  }
+
 }
