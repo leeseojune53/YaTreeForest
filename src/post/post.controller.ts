9607fad1f0279f6caf95bdf86c85267eb0a2b6a2
leeseojune <sung07288346@gmail.com>
2021-03-25 23:26:07 +0900

    UPDATE; PostController

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, UseGuards } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { PostDto } from './dto/post.dto';
+import { PostService } from './post.service';
 
 @Controller('post')
 export class PostController {
+    constructor(
+        private postService: PostService,
+    ){}
+
+    @Post()
+    write(@Body() request: PostDto) {
+        this.postService.writePost(request);
+    }
 
 }
