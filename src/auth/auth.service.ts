a01e81dffae7b5e17816ddcd0cf5e030e270677e
leeseojune <sung07288346@gmail.com>
2021-03-25 20:13:10 +0900

    CREATE; AuthService

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/entity/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private userService: UserService,
+        private jwtService: JwtService,
+    ){}
+
+    async validateUser(userName: string, pass: string) {
+        const user = await this.userService.findOne(userName);
+        if(user && bcrypt.compare(pass, user.password)){
+            const {password, ...result} = user;
+            return user;
+        }
+        return null;
+    }
+
+    async login(userName: string, pass: string) {
+        var user: User = await this.validateUser(userName, pass);
+        const payload = { sub: user.id };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
+    }
+}
