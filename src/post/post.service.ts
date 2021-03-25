d877669cd8bc69709c4bb546ed9d7a9e8f24f238
leeseojune <sung07288346@gmail.com>
2021-03-25 23:26:32 +0900

    UPDATE; PostService

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Post } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PostRepository } from './entity/post.repo'
+import { PostDto } from './dto/post.dto';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+    constructor(
+        @InjectRepository(Post)
+        private postRepository: PostRepository,
+    ){}
+
+    writePost(request: PostDto) {
+        this.postRepository.savePost(request);
+    }
+
+}
