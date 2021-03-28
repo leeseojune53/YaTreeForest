e17d7f1eb8dd68185494d38b62bd717b7ee56ef6
leeseojune <sung07288346@gmail.com>
2021-03-28 23:43:56 +0900

    UPDATE; PostModule

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -3,9 +3,10 @@ import { PostController } from './post.controller';
 import { PostRepository } from './entity/post.repo';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostService } from './post.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostRepository])],
+  imports: [TypeOrmModule.forFeature([PostRepository]), AuthModule],
   providers: [PostService],
   controllers: [PostController]
 })
