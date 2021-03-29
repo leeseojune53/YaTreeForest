ce63c6ded9d0c6c031d190a56d585dff9c7495a4
leeseojune <sung07288346@gmail.com>
2021-03-29 19:18:19 +0900

    UPDATE; UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -29,6 +29,10 @@ export class UserService {
         return this.userRepository.findOne({userName});
     }
 
+    findById(userId: number) {
+        return this.userRepository.findOne({id: userId});
+    }
+
     
 
 }
