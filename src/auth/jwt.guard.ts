d79c5389a8c70960c288ac394c257268a96901a9
leeseojune <sung07288346@gmail.com>
2021-03-29 19:17:51 +0900

    UPDATE; JwtGuard

--- src/auth/jwt.guard.ts
+++ src/auth/jwt.guard.ts
@@ -3,16 +3,16 @@ import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService){}
+export class JwtGuard implements CanActivate {
+  constructor(
+    private authService: AuthService
+  ){}
 
-  canActivate(context: ExecutionContext,): boolean {
+  canActivate(context: ExecutionContext,){
     const request: Request = context.switchToHttp().getRequest();
     const token: string = request.headers['authorization'];
     this.authService.validateToken(token.split(" ")[1]);
 
     return true;
   }
-
-
 }
