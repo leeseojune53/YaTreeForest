2c5fbd0e69e58e9bc9ec62ae068285cf0526d4e2
leeseojune <sung07288346@gmail.com>
2021-03-31 20:17:00 +0900

    UPDATE; UserController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
 import { UserDto } from './dto/user.dto';
 import { UserService } from './user.service';
 
@@ -6,11 +7,17 @@ import { UserService } from './user.service';
 export class UserController {
     constructor(
         private readonly userService:UserService,
+        private readonly authService:AuthService,
     ){}
 
     @Post('register')
     register(@Body() request: UserDto){
         this.userService.register(request);
+    }
+
+    @Post('login')
+    login(@Body() request: UserDto) {
+        return this.authService.login(request.userName, request.password);
     }
 
 }
